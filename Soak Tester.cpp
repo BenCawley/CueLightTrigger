d4f80736bc2b8956a895e5e6911ea8c3e9fc0bda
Zoarial92 <34109762+Zoarial92@users.noreply.github.com>
2019-12-13 16:37:55 +0000

    Changed an error in Soak Tester where randomSeed was trying digitalRead instead of analogRead a pin

--- Soak Tester.cpp
+++ Soak Tester.cpp
@@ -1,9 +1,10 @@
 //A short script running on another arduino to soak test the CueLightTrigger box inputs 
 
-int outPins = {2, 3, 4, 5};
+int outPins[4] = {2, 3, 4, 5};
 int inPins = A0;
 
 void setup() {
+    Serial.begin(9600);
     for (int i = 0; i < 4; i++) {
         pinMode(outPins[i], OUTPUT);
         digitalWrite(outPins[i], HIGH);
@@ -12,11 +13,16 @@ void setup() {
 }
 
 void loop() {
-    randomSeed(digitalRead(inPins));
+    randomSeed(analogRead(inPins));
     int randNum = random(5);
     int randTime = random(100, 5000);
 
     digitalWrite(outPins[randNum], LOW);
+    Serial.print("\nChannel: ");
+    Serial.print(randNum);
+    Serial.print("\nDelay: ");
+    Serial.print(randTime);
+    Serial.print("\n");
     delay(randTime);
     digitalWrite(outPins[randNum], HIGH);
 }
